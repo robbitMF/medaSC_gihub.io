61ea79ce9fb5591c4b7a8e1fe7e9a981213a95de
robbitMF <134674592+robbitMF@users.noreply.github.com>
2026-07-25 10:14:19 +0900

    add fix

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55405671,
+    wasmFileSize: 58124828,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
